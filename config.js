da2cc269cd566557813b62297378371705f953ee
Jesús López <inyenia@gmail.com>
2014-06-15 19:29:47 +0200

    configure Data Base

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /**
 * Config file for the API
 */
-exports.db_url = 'mongodb://localhost:27017/angular-todo';
+exports.db_url = 'mongodb://nodejitsu:578034675bfc4476b20009be72c0c62f@troup.mongohq.com:10038/nodejitsudb3646634265';
